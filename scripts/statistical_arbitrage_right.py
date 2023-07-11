c00d16973e1f5dfcad8ee80a963c503b639832b3
doidoi <wilfredlsc@gmail.com>
2023-07-12 07:20:35 +0800

    (edit) added left and right side of the arbitrage

--- scripts/statistical_arbitrage_right.py
+++ scripts/statistical_arbitrage_right.py
@@ -7,18 +7,32 @@ from hummingbot.data_feed.candles_feed.candles_factory import CandlesFactory
 from hummingbot.strategy.directional_strategy_base import DirectionalStrategyBase
 
 
-class StatisticalArbitrage(DirectionalStrategyBase):
-    directional_strategy_name: str = "statistical_arbitrage"
+class StatisticalArbitrageRight(DirectionalStrategyBase):
+    """
+        BotCamp Cohort #5 July 2023
+        Design Template: https://github.com/hummingbot/hummingbot-botcamp/issues/48
+
+        Description:
+        Statistical Arbitrage strategy implementation based on the DirectionalStrategyBase.
+        This strategy execute trades based on the Z-score values.
+        This strategy is divided into a left and right side code.
+        Left side code is statistical_arbitrage_left.py.
+        Right side code is statistical_arbitrage_right.py.
+        This code the right side of this strategy
+        When z-score indicates an entry signal. the left side will execute a long position and right side will execute a short position.
+        When z-score indicates an exit signal. the left side will execute a short position and right side will execute a long position.
+        """
+    directional_strategy_name: str = "statistical_arbitrage_right"
     # Define the trading pair and exchange that we want to use and the csv where we are going to store the entries
-    trading_pair_left: str = "ETH-USDT"
-    trading_pair: str = "BTC-USDT"
+    trading_pair_left: str = "ETH-USDT"  # left side trading pair
+    trading_pair: str = "BTC-USDT"  # right side trading pair
     exchange: str = "binance_perpetual"
     order_amount_usd = Decimal("15")
     leverage = 10
     length = 100
 
     # Configure the parameters for the position
-    zscore_entry: int = -2  #
+    zscore_entry: int = -2
     zscore_entry_sl: int = -3
     zscore_exit: int = 2
     zscore_exit_sl: int = 3
@@ -39,13 +53,13 @@ class StatisticalArbitrage(DirectionalStrategyBase):
         z_score = candles_df.iat[-1, -1]
 
         if z_score < self.zscore_entry:
-            return 0  # short -1
+            return -1  # short -1
         elif z_score < self.zscore_entry_sl:
-            return 0  # stop loss short 1
+            return 1  # stop loss short 1
         elif z_score > self.zscore_exit:
-            return 0  # long 1
+            return 1  # long 1
         elif z_score > self.zscore_exit_sl:
-            return 0  # stop loss long -1
+            return -1  # stop loss long -1
         else:
             return 0
 
@@ -54,6 +68,7 @@ class StatisticalArbitrage(DirectionalStrategyBase):
         candles_df_1 = self.candles[0].candles_df
         candles_df_2 = self.candles[1].candles_df
 
+        # calculate the spread and z-score based on the candles of 2 trading pairs
         df = pd.merge(candles_df_1, candles_df_2, on="timestamp", how='inner', suffixes=('_left', ''))
         hedge_ratio = df["close_left"].tail(self.length).mean() / df["close"].tail(self.length).mean()
 
@@ -63,7 +78,11 @@ class StatisticalArbitrage(DirectionalStrategyBase):
         return df
 
     def market_data_extra_info(self):
-
+        """
+        Provides additional information about the market data to the format status.
+        Returns:
+            List[str]: A list of formatted strings containing market data information.
+        """
         lines = []
         columns_to_show = ["timestamp", "open", "low", "high", "close", "volume", "z_score"]
         candles_df = self.get_processed_df()
